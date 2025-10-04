54ccca8b06a73231555051d753e5ce72fc7dd926
Alison G. Martins <odevalison@gmail.com>
2025-10-04 02:23:31 -0300

    chore(ui): add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/button'
+import { Input } from '@/components/input'
+import logo from '/public/logo-pizzaria.svg'
 import styles from './page.module.scss'
 
 const HomePage = () => {
   return (
-    <main>
-      <h1 className={styles.test}>Página login</h1>
-    </main>
+    <div className={styles.container}>
+      <Image src={logo} width={335} alt='Logo pizzaria dev' />
+
+      <section className={styles.login}>
+        <form>
+          <Input
+            type='email'
+            name='email'
+            placeholder='Digite seu e-mail'
+            required
+          />
+          <Input
+            type='password'
+            name='password'
+            placeholder='Digite sua senha'
+            required
+          />
+          <Button type='submit'>Acessar</Button>
+        </form>
+
+        <p className={styles.signupText}>
+          Ainda não criou uma conta?{' '}
+          <Link href='/sign-up' className={styles.signupLink}>
+            Cadastre-se.
+          </Link>
+        </p>
+      </section>
+    </div>
   )
 }
 
