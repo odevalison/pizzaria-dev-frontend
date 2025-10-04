656da51abe59d732adcaa6df64cb2d6e3842d4e5
Alison G. Martins <odevalison@gmail.com>
2025-10-04 03:04:11 -0300

    chore(ui): add common folder for shared components

---
+++ src/components/common/button/index.tsx
@@ -0,0 +1,26 @@
+import { Poppins } from 'next/font/google'
+import type { ComponentProps, ReactNode } from 'react'
+import styles from './button.module.scss'
+
+interface ButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+  type?: ComponentProps<'button'>['type']
+}
+
+const poppins = Poppins({ subsets: ['latin'], weight: '600' })
+
+export const Button = ({
+  children,
+  type = 'button',
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`${styles.button} ${poppins.className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
---
+++ src/components/common/input/index.tsx
@@ -0,0 +1,34 @@
+import { Poppins } from 'next/font/google'
+import { type ComponentProps, useId } from 'react'
+import styles from './input.module.scss'
+
+interface InputProps extends ComponentProps<'input'> {
+  label?: string
+  placeholder: string
+  name: string
+}
+
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
+
+export const Input = ({ label, placeholder, name, ...props }: InputProps) => {
+  const inputId = useId()
+
+  return (
+    <>
+      {!!label && (
+        <label htmlFor={inputId} className={styles.label}>
+          {label}
+        </label>
+      )}
+
+      <input
+        className={`${styles.input} ${poppins.className}`}
+        id={inputId}
+        placeholder={placeholder}
+        name={name}
+        autoComplete='off'
+        {...props}
+      />
+    </>
+  )
+}
