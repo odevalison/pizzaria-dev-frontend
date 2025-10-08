cab7306706b3dfbc258d93c4c380e748093ae349
Alison G. Martins <odevalison@gmail.com>
2025-10-08 20:16:51 -0300

    chore: rename and move signup, login and types files

--- src/actions/create-user/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const createUserSchema = z.object({
-  name: z.string().min(1, 'Nome obrigatório.'),
-  email: z.email('Digite um e-mail válido.').min(1, 'E-mail obrigatório.'),
-  password: z.string('Senha inválida').min(8, 'Senha muito fraca.')
-})
-
-export type CreateUserData = z.infer<typeof createUserSchema>
